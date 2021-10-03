6701c0e3328ab48e8e1292397fe43c0759035daf
AdinaFakih <addy.fakih@gmail.com>
2021-10-03 22:03:52 +0500

    Constructor and toString method

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,22 @@
-
+//demo for toString method
 public class Customer {
+	//instance VAR
+	String name;
+		
+	//constructor
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	//return name of constructor 
+	public String toString() {
+		return this.name;       //to be printed
+	}
+	
+	public static void main(String[] args) {
+		Customer c =  new Customer("Adina");
+		System.out.println("c = " + c);  //Customer c into a string, calling to toString method 
+		
+	}
 
 }
